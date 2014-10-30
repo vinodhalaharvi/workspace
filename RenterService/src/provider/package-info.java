3e869b27302a6cc990d100834da7fb0820ed9b4f
Vinod Halaharvi <vinod.halaharvi@gmail.com>
2014-10-30 22:12:54 +0000

    changes

--- RenterService/src/provider/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * 
- */
-/**
- * @author s2687
- *
- */
-package provider;
--- RenterService/test.py
+++
@@ -1,9 +0,0 @@
-import sys
-for line in sys.stdin.readlines():
-	line = line.strip()
-	one, two = line.split()
-	if sys.argv[1] == "method":
-		print "+ %s:%s" % (one, two)
-	elif sys.argv[1] == "prop":
-		print "- %s:%s" % (one, two)
-
