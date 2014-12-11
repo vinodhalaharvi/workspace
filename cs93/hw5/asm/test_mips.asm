bdad0e52c069a257cc2291ac64ec78cc92faf61a
Vinod Halaharvi <vinod.halaharvi@gmail.com>
2014-12-11 05:42:50 +0000

    starting to port to hardware

--- cs93/hw5/asm/test_mips.asm
+++ cs93/hw5/asm/test_mips.asm
@@ -28,34 +28,19 @@
 		syscall
 		li $v0, 10 
 		syscall
-main:
-	addi $a0, $zero, 11
-	addi $a1, $zero, 12
-	slt  $t4, $a0, $zero # check if the first number is negative beq  $t4, $zero, skip1
-	sub  $a0, $zero, $a0 # flip sign 
-	skip1:
-	slt  $t5, $a1, $zero # check if the second number is negative 
-	beq  $t5, $zero, skip2
-	sub  $a1, $zero, $a1 # flip sign 
-	skip2:
-	add $t0, $zero, $zero
-	addi $v0, $zero, 0 # initialize $v0
-	multiplyloop: 
-		srav  $t1, $a1, $t0 # shift multiplier to right
-		andi  $t2, $t1, 1
-		addi  $t1, $zero, 1
-		beq   $t2, $t1 label 
-		back:
-			addi  $t0, $t0, 1
-			slti  $t8, $t0, 32 # shift multiplicand to left
-			bne   $t8, $zero, multiplyloop	
-			# fix sign before returning
-			beq $t4, $zero, skip3 # first number was negative
-			sub  $v0, $zero, $v0 # flip sign 
-			skip3:
-				beq $t5, $zero, label # second number was negative
-				sub  $v0, $zero, $v0 # flip sign 
-		label:
-			sllv $t3, $a0, $t0 # shift left by the amount in $a0
-			add $v0, $v0, $t3 # accumulator
-	jal printAndExit
+	jal main
+	main:
+		addi $t0, $zero, 10
+		addi $t1, $zero, 5
+		addi $t3, $zero, 2
+		sub  $t2, $t0, $t1
+		addi $t1, $zero, 5
+		sll  $t1, $t0, 3 
+		sllv $t2, $t0, $t3
+		sllv $t2, $t0, $t3
+		slti $t1, $t2, 10
+		sllv $t2, $t0, $t3
+		srav $t1, $t2, $t3
+		srl  $t1, $t0, 5
+		srav $t1, $t2, $t3
+		jal printAndExit
